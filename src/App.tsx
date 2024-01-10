450fa0a41ce24fcd4c9ac201101ff64d1f84c39e
Maria <m.krivetz@gmail.com>
2024-01-10 17:58:46 +0200

    fetch google

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { GetUsersDocument } from './graphql/generated'
 // import { User } from './types'
 // import { Message } from './types'
 import UserDisplay from './components/UserDisplay'
+import { useEffect } from 'react'
 // import MessageDisplay from './components/MessageDisplay'
 
 
@@ -11,12 +12,22 @@ function App() {
     query: GetUsersDocument
   })
 
+  useEffect(() => {
+    fetch('https://sheetdb.io/api/v1/zps9tswoyr8zp')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  }, [])
+
   return (
     <>
       <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-start p-4 gap-y-12 overflow-scroll">
       {
         results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
       }
+</div>
+
+<div>
+
 </div>
 
     </>
